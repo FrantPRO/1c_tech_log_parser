e02b2235fab1a6c1626848ee932e68f388add1a3
Stas <stas.frantsuzov@mail.ru>
2019-01-06 18:12:25 +0300

    Fix

--- parser.py
+++ parser.py
@@ -15,17 +15,29 @@ def read_log(str_directory, str_file):
     log_date = str_file[4:6] + "." + str_file[2:4] + ".20" + str_file[0:2] + " " + str_file[6:8] + ":"
 
     with open(os.path.join(str_directory, str_file), 'r') as f:
+
         for line in f:
-            m_params = line.split(',')
-            d_result = {"Date": log_date + m_params[0][0:5], "Event": m_params[1], "Level": m_params[2]}
-            del (m_params[0:3])
-            for element in m_params:
-                el = element.split('=')
-                if len(el) == 1:
-                    d_result[el[0]] = el[0]
-                else:
-                    d_result[el[0]] = el[1]
-            m_result.append(d_result)
+            # print(line)
+
+            if "meta" in line:
+
+                m_params = line.split(',')
+
+                d_result = {"Date": log_date + m_params[0][0:5], "Event": m_params[1], "Level": m_params[2]}
+
+                del (m_params[0:3])
+
+                for element in m_params:
+
+                    # Получаем параметры
+                    el = element.split('=')
+                    if len(el) == 1:
+                        d_result[el[0]] = el[0]
+                    else:
+                        d_result[el[0]] = el[1]
+
+                m_result.append(d_result)
+
     return m_result
 
 
