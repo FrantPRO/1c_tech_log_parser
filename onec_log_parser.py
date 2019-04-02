53fc187b7de48e166a48029ca8fae01857c1f891
Stas <stas.frantsuzov@mail.ru>
2019-04-02 15:48:55 +0300

    update

--- onec_log_parser.py
+++ onec_log_parser.py
@@ -10,10 +10,29 @@ directory = os.path.normpath(r'\\1cv8\logs')
 selection = "meta"
 
 
+# функция открывает файл и читает по строке с контрлем конца строки
+def line_reader(file_path):
+    with open(file_path, 'r', encoding='utf-8-sig') as f:
+        complite_line = ''
+        for line in f:
+            if not line:
+                continue
+            complite_line += line
+            if ':\n' in complite_line:
+                complite_line = line.strip('\n')
+                continue
+            else:
+                yield complite_line.strip('\n')
+                complite_line = ''
+
+
 # Метод возвращает словарь с параметрами из файла
 # Формат папок логов <ИмяПроцесса>_<ИдентификаторПроцесса>
 # Формат фалов логов: имя файла = ГГММДДЧЧ.log, начало файла ММ:СС.тттт-д,<ИмяСобытия>,<Уровень>,<Ключ=Значение>,...
 def read_log_file(str_directory, str_file):
+
+    if len(str_file) != 12:
+        return 0
 
     m_result = {}
 
@@ -21,34 +40,38 @@ def read_log_file(str_directory, str_file):
     log_date = str_file[4:6] + "." + str_file[2:4] + ".20" + str_file[0:2]
     log_hour = str_file[6:8] + ":"
 
-    with open(os.path.join(str_directory, str_file), 'r', encoding='UTF-8') as f:
+    dm_result = []
 
-        dm_result = []
+    # Читаем файл по строкам
+    for line in line_reader(os.path.join(str_directory, str_file)):
 
-        # Читаем файл по строкам
-        for line in f:
+        # Если искомое значение есть в строке
+        if selection in line:
 
-            # Если искомое значение есть в строке
-            if selection in line:
+            # Получаем основные параметры
+            m_params = line.split(',')
+            time = log_hour + m_params[0][0:5]
+            rphost = str_directory.split('_')[-1] if len(str_directory.split('_')) > 1 else ''
 
-                # Получаем основные параметры
-                m_params = line.split(',')
-                d_result = {"Time": log_hour + m_params[0][0:5], "Event": m_params[1], "Level": m_params[2]}
-                del (m_params[0:3])
+            d_result = {"Time": time, "Event": m_params[1], "Level": m_params[2], "rphost": rphost}
 
-                # Получаем остальные параметры
-                o_params = {}
-                for element in m_params:
-                    el = element.split('=')
-                    if len(el) == 1:
-                        o_params[el[0]] = el[0]
-                    else:
-                        o_params[el[0]] = el[1].strip()
-                d_result["Params"] = o_params
+            # Удаляем основные параметры, чтобы не мешали
+            del (m_params[0:3])
 
-                dm_result.append(d_result)
+            # Получаем дополнительные параметры
+            o_params = {}
+            for element in m_params:
+                element = element.strip()
+                el = element.split('=')
+                if len(el) == 1:
+                    o_params[el[0]] = el[0]
+                else:
+                    o_params[el[0]] = el[1].strip()
+            d_result["Params"] = o_params
 
-                m_result["File_" + log_date] = dm_result
+            dm_result.append(d_result)
+
+            m_result["File_" + log_date] = dm_result
 
     return m_result
 
@@ -67,5 +90,4 @@ for d, dirs, files in os.walk(directory):
         if log_file:
             result.append(log_file)
 
-print(json.dumps(result))
-
+print(json.dumps(result, ensure_ascii=False))
